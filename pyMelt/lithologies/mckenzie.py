4eb0caa4982a2d39abc56ea2e57a833aa474891e
Kev Wong <68993457+kev-wong-geo@users.noreply.github.com>
2021-10-05 14:12:38 +0100

    Update mckenzie.py
    Formatted using flake8

--- pyMelt/lithologies/mckenzie.py
+++ pyMelt/lithologies/mckenzie.py
@@ -13,11 +13,12 @@ import numpy as np
 from scipy.optimize import fsolve
 from scipy.special import expit
 
+
 class garnet_peridotite(_lithology):
     """
     Implementation of the McKenzie and Bickle (1988) garnet peridotite melting model.
     As this parameterisation provides pressure as a function of solidus temperature,
-    scipt.optimize.fsolve and scipy.special.expit are required to numerically find 
+    scipt.optimize.fsolve and scipy.special.expit are required to numerically find
     solidus temperature as a function of pressure.
     To use the same format of parameterisation for another lithology, the parameter values
     may be changed. They are provided as a dictionary during initialisation of the class, with
@@ -83,9 +84,9 @@ class garnet_peridotite(_lithology):
         Calculates the solidus temperature, at a given pressure, using Equation 18 of
         McKenzie and Bickle (1988). Requires scipy.optimize.fsolve to solve for pressure.
         As fsolve can only solve for a single pressure TSolidus first detects whether P is
-        given as a list, tuple, numpy.ndarray (e.g. numpy.linspace), or scalar prior to 
+        given as a list, tuple, numpy.ndarray (e.g. numpy.linspace), or scalar prior to
         calculating the solidus pressure.
-        
+
         Parameters
         ----------
         P : float
@@ -95,27 +96,29 @@ class garnet_peridotite(_lithology):
         float
             Solidus temperature (degC).
         """
-
         TSolidus_initial_guess = 1300.0
-        TSolidus_solution = []
-        if isinstance(P,(list, tuple, np.ndarray)) is True:
+
+        def func(TSolidus):
+            _func = ((TSolidus - self.parameters['A1']) / self.parameters['A2']
+                     + self.parameters['A3'] * np.exp(self.parameters['A4']
+                     * (TSolidus - self.parameters['A1']))) - Pressure
+            return _func
+
+        if isinstance(P, (list, tuple, np.ndarray)) is True:
+            TSolidus_solution = []
             for i in range(len(P)):
-                func = lambda TSolidus: ((TSolidus - self.parameters['A1']) / self.parameters['A2'] 
-                                         + self.parameters['A3'] * np.exp(self.parameters['A4']
-                                         * (TSolidus - self.parameters['A1']))) - P[i]
+                Pressure = P[i]
                 TSolidus_solution.append(fsolve(func, TSolidus_initial_guess, maxfev=50000)[0])
         else:
-            func = lambda TSolidus: ((TSolidus - self.parameters['A1']) /  self.parameters['A2']
-                                     + self.parameters['A3'] * np.exp(self.parameters['A4']
-                                     * (TSolidus - self.parameters['A1']))) - P
+            Pressure = P
             TSolidus_solution = fsolve(func, TSolidus_initial_guess, maxfev=50000)[0]
         return TSolidus_solution
-    
+
     def TLiquidus(self, P):
         """
         Calculates the liquidus temperature, at a given pressure, using Equation 19 of
-        McKenzie and Bickle (1988). 
-        
+        McKenzie and Bickle (1988).
+
         Parameters
         ----------
         P : float
@@ -128,13 +131,13 @@ class garnet_peridotite(_lithology):
         TLiquidus = (self.parameters['B1'] + self.parameters['B2'] * P + self.parameters['B3']
                      * np.arctan(P / self.parameters['B4']))
         return TLiquidus
-    
-    def _dTdPSolidus(self, P): 
+
+    def _dTdPSolidus(self, P):
         """
-        Returns the solidus temperature gradient at any given pressure. Requires 
-        scipy.optimize.fsolve to solve for pressure, and scipy.special.expit to 
+        Returns the solidus temperature gradient at any given pressure. Requires
+        scipy.optimize.fsolve to solve for pressure, and scipy.special.expit to
         avoid exponential overflows that can occur with numpy.exp.
-        
+
         Parameters
         ----------
         P : float
@@ -144,17 +147,16 @@ class garnet_peridotite(_lithology):
         float
             Solidus temperaure gradient (degC/GPa)
         """
-        
         TSolidus = self.TSolidus(P)
-        dTdPSolidus = (1 / (1 / self.parameters['A2'] + self.parameters['A3'] 
-                       * self.parameters['A4'] * expit(self.parameters['A4'] 
+        dTdPSolidus = (1 / (1 / self.parameters['A2'] + self.parameters['A3']
+                       * self.parameters['A4'] * expit(self.parameters['A4']
                        * (TSolidus - self.parameters['A1']))))
         return dTdPSolidus
 
     def _dTdPLiquidus(self, P):
         """
         Returns the liquidus temperature gradient at any given pressure.
-        
+
         Parameters
         ----------
         P : float
@@ -165,14 +167,14 @@ class garnet_peridotite(_lithology):
             Solidus temperaure gradient (degC/GPa)
         """
         dTdPLiquidus = (self.parameters['B2'] + self.parameters['B3'] /
-                        (1 + (P/self.parameters['B4'])**2))
+                        (1 + (P / self.parameters['B4'])**2))
         return dTdPLiquidus
-    
-    def _RescaledT(self, T, P): 
+
+    def _RescaledT(self, T, P):
         """
-        Calculates the rescaled temperature defined by Equation 20 of 
+        Calculates the rescaled temperature defined by Equation 20 of
         McKenzie and Bickle (1988).
-        
+
         Parameters
         ----------
         T : float
@@ -188,13 +190,13 @@ class garnet_peridotite(_lithology):
         TLiquidus = self.TLiquidus(P)
         RescaledT = (T - (TSolidus + TLiquidus) / 2) / (TLiquidus - TSolidus)
         return RescaledT
-    
-    def F(self, P, T): 
+
+    def F(self, P, T):
         """
-        Wrapper for the melt fraction functions, defined by Equations 21 and 22 
-        of McKenzie and Bickle (1988). If T and P are below the solidus, returns 0, 
+        Wrapper for the melt fraction functions, defined by Equations 21 and 22
+        of McKenzie and Bickle (1988). If T and P are below the solidus, returns 0,
         if they are above the liquidus, returns 1. Otherwise determines F.
-        
+
         Parameters
         ----------
         P : float
@@ -223,7 +225,7 @@ class garnet_peridotite(_lithology):
         Calculates dT/dF(const. P). First calculates the melt fraction. If F is
         zero, returns _np.inf. If F is 1, returns _np.inf. Otherwise uses the
         appropriate expression for melt fraction.
-        
+
         Parameters
         ----------
         P : float
@@ -245,14 +247,14 @@ class garnet_peridotite(_lithology):
         else:
             dFdT = (((1 - 0.25 * self.parameters['a1']) + 3 * self.parameters['a0'] * RescaledT
                      + 3 * self.parameters['a1'] * RescaledT**2) / (TLiquidus - TSolidus))
-            dTdF = 1/dFdT
+            dTdF = 1 / dFdT
         return dTdF
-    
+
     def dTdP(self, P, T):
         """
         Calculates dT/dP(const. F). First calculates F, then chooses the
         appropriate expression for melt fraction.
-        
+
         Parameters
         ----------
         P:  float
