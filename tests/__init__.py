d66a270a8d47ee80e0ab5b1ef12b047acca03824
Simon Matthews <simonwmatthews@gmail.com>
2021-11-24 11:36:29 +0000

    Added readthedocs yaml file

---
+++ tests/__init__.py
@@ -0,0 +1,6 @@
+"""
+pyMelt testing routines
+
+The modules contained here allow automated testing of pyMelt's functions to ensure correct
+installation and non-destructive updates.
+"""
---
+++ tests/test_lithologies.py
@@ -0,0 +1,8 @@
+"""
+Lithology Tests
+
+This module provides testing routines for the pure lithology classes.
+"""
+
+import unittest
+import pyMelt as m
