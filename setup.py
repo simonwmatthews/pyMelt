6384374eadc98576fdc28b85598e08f818c58468
Simon Matthews <simonwmatthews@gmail.com>
2022-01-17 11:31:03 +0000

    Incremented version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyMelt",
-    version="1.930",
+    version="1.931",
     author="Simon Matthews, Kevin Wong, Matthew Gleeson",
     author_email="simonwmatthews@gmail.com",
     description=("A python library for calculating the melting behaviour of Earth's mantle."),
