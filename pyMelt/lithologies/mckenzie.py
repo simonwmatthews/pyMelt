5ae9c121aa19453e9b8050ebcf3d1dccac7375d0
Kevin Wong <68993457+kev-wong-geo@users.noreply.github.com>
2026-03-05 14:29:06 -0500

    Update import for derivative function from scipy

--- pyMelt/lithologies/mckenzie.py
+++ pyMelt/lithologies/mckenzie.py
@@ -13,9 +13,9 @@ from scipy.optimize import fsolve as _fsolve
 from scipy.special import expit as _expit
 
 try:
-    from scipy.misc import derivative as _derivative
+    from scipy.differentiate import derivative as _derivative
 except Exception:
-    from scipy._lib._finite_differences import _derivative as _derivative
+    from scipy._lib._differentiate import _derivative as _derivative
 from scipy.optimize import root_scalar as _root_scalar
 
 
